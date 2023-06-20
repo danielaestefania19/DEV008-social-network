c41be8453b5001478d0d3557e098cbddcd8451f8
esthefaniagv <esthefaniagv@gmail.com>
2023-06-20 14:18:14 -0400

    arreglo errores codigo

--- src/components/inicio.js
+++
@@ -1,5 +0,0 @@
-export const logIn = (onNavigate) => {
-    const logMainSection = document.createElement('section');
-    logMainSection.setAttribute('id', 'idlogin');
-    const logInCont = document.createElement('div');
-    logInCont.classList.add('loginContainer');
