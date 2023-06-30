a66046dde5407fe35f223f94a82aa35d2fb64bb0
Arely Gonzalez <arely.web@gmail.com>
2023-06-30 10:49:11 -0600

    creacion responsive web pagina register

--- src/components/register.js
+++ src/components/register.js
@@ -1,53 +1,53 @@
+//import { registrarUsuario, actualizaPerfil } from '../lib/firebase';
+
 export const register = (onNavigate) => {
   const logMainSection = document.createElement('section');
-  logMainSection.setAttribute('id', 'idlogin');
+  logMainSection.setAttribute('id', 'idRegister');
+  logMainSection.classList.add('sectionReg');
   const logInCont = document.createElement('div');
-  logInCont.classList.add('loginContainer');
+  logInCont.classList.add('registerContainer');
+  const rsgisterImagenCont = document.createElement('div');
+  rsgisterImagenCont.classList.add('registerImagenContainer');
 
   const containerLogo = document.createElement('div');
-  containerLogo.classList.add('loginContainer__logo');
+  containerLogo.classList.add('registerContainer__logo');
   const logo = document.createElement('img');
-  logo.classList.add('loginContainer__logo__img');
+  logo.classList.add('registerContainer__logo__img');
   logo.setAttribute('src', 'img/logoNegativo.png');
   const slogan = document.createElement('p');
-  slogan.classList.add('loginContainer__logo__label');
+  slogan.classList.add('registerContainer__logo__label');
   slogan.innerHTML = 'Conecta, emprende e inspira.';
 
   containerLogo.appendChild(logo);
   containerLogo.appendChild(slogan);
 
   const containerInput = document.createElement('div');
-  containerInput.classList.add('loginContainer__inputs');
+  containerInput.classList.add('registerContainer__inputs');
   containerInput.setAttribute('id', 'regContainer');
   const email = document.createElement('input');
   email.setAttribute('type', 'text');
   email.setAttribute('placeholder', 'Correo electrónico');
   email.setAttribute('id', 'idEmail');
-  email.classList.add('loginContainer__inputs__text');
+  email.classList.add('registerContainer__inputs__text');
   const pass = document.createElement('input');
   pass.setAttribute('type', 'text');
   pass.setAttribute('placeholder', 'Contraseña');
   pass.setAttribute('id', 'idPassword');
-  pass.classList.add('loginContainer__inputs__text');
-  const verifyPass = document.createElement('input');
-  verifyPass.setAttribute('type', 'text');
-  verifyPass.setAttribute('placeholder', 'Verificar contraseña');
-  verifyPass.setAttribute('id', 'idPasswordVerify');
-  verifyPass.classList.add('loginContainer__inputs__text');
+  pass.classList.add('registerContainer__inputs__text');
+  
 
   containerInput.appendChild(email);
   containerInput.appendChild(pass);
-  containerInput.appendChild(verifyPass);
 
   const containerBtns = document.createElement('div');
-  containerBtns.classList.add('loginContainer__botones');
+  containerBtns.classList.add('registerContainer__botones');
   const createUser = document.createElement('button');
-  createUser.classList.add('loginContainer__botones__ing');
+  createUser.classList.add('registerContainer__botones__ing');
   createUser.textContent = 'Crear cuenta';
   const labelO = document.createElement('label');
   labelO.textContent = 'o';
   const gooBtnCreate = document.createElement('button');
-  gooBtnCreate.classList.add('loginContainer__botones__goo');
+  gooBtnCreate.classList.add('registerContainer__botones__goo');
   gooBtnCreate.textContent = 'Crear cuenta con Google';
   const gooImg = document.createElement('img');
   gooImg.setAttribute('src', 'img/goo.png');
@@ -55,35 +55,62 @@ export const register = (onNavigate) => {
   const createAccount = document.createElement('p');
   const noAccount = document.createElement('label');
   noAccount.textContent = '¿Ya tienes cuenta?';
-  createAccount.textContent = '¡Ingresa!';
-  createAccount.appendChild(noAccount);
+  const IngresaT = document.createElement('a');
+  IngresaT.textContent = '¡Ingresa!';
 
+  IngresaT.addEventListener('click', () => onNavigate('/'));
+  createUser.addEventListener('click', ()=>{ 
+    const email= document.getElementById("idEmail").value;
+    const pass = document.getElementById("idPassword").value;
+    localStorage.setItem("nameStorage",document.getElementById("idNameUser").value);
+    registrarUsuario(email,pass).then(function(response) {
+       if(response.user.email !== null){
+       
+         onNavigate('/inicio');
+       }
+
+    }).catch(function(error) {
+     const errorCode = error.code;
+         const errorMessage = error.message;
+         console.log(errorCode, errorMessage);
+    });
+  
+  
+ });
+
+  
   containerBtns.appendChild(createUser);
   containerBtns.appendChild(labelO);
+  createAccount.appendChild(noAccount);
+  createAccount.appendChild(IngresaT);
   containerBtns.appendChild(gooBtnCreate);
   containerBtns.appendChild(createAccount);
 
   createUser.addEventListener('click', () => onNavigate('/'));
 
   const containertImg = document.createElement('div');
-  containertImg.classList.add('loginContainer__idImagen');
+  containertImg.classList.add('registerImagenContainer__idImagen');
   const imgBoun = document.createElement('img');
-  imgBoun.classList.add('loginContainer__idImagen__boun');
-  imgBoun.setAttribute('src', 'img/flower_peakingRegister.png');
+  imgBoun.classList.add('registerImagenContainer__idImagen__boun');
+  imgBoun.setAttribute('src', 'img/flower_sprout(verde).png');
   const imgMujer = document.createElement('img');
-  imgMujer.classList.add('loginContainer__idImagen__reg');
-  imgMujer.setAttribute('src', 'img/profileRegister.png');
+  imgMujer.classList.add('registerImagenContainer__idImagen__reg');
+  imgMujer.setAttribute('src', 'img/pwm-register.png');
 
   containertImg.appendChild(imgBoun);
   containertImg.appendChild(imgMujer);
+  
+
 
   // Agregar todos los div al div principal
   logInCont.appendChild(containerLogo);
   logInCont.appendChild(containerInput);
   logInCont.appendChild(containerBtns);
-  logInCont.appendChild(containertImg);
+  rsgisterImagenCont.appendChild(containertImg);
 
+ 
   logMainSection.appendChild(logInCont);
+  logMainSection.appendChild(rsgisterImagenCont);
 
   return logMainSection;
 };
