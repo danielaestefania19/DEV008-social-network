5e92c62f3f5809039567df6cde9f76dc09ed95fd
danielaestefania19 <danielacastellano1000@gmail.com>
2023-07-21 09:20:45 -0600

    Intento test

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   extends: 'stylelint-config-recommended',
+  rules: {
+    'declaration-block-no-duplicate-properties': null,
+  },
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,73 +1,18 @@
+/* eslint-disable indent */
+/* eslint-disable no-trailing-spaces */
 /* eslint-disable arrow-body-style */
-// importamos la funcion que vamos a testear
-// import { myFunction } from '../src/lib/index';
-
-// describe('myFunction', () => {
-//   it('debería ser una función', () => {
-//     expect(typeof myFunction).toBe('function');
-//   });
-// });
-// import {
-//   iniciaSesionUsuario,
-//   registrarUsuario,
-//   // inicioGoogle,
-//   // salirSesion,
-//   // actualizaPerfil,
-// } from '../src/lib/firebase';
-
-// // Mock de las funciones de Firebase
-// jest.mock('../src/lib/firebase', () => ({
-//   iniciaSesionUsuario: jest.fn(),
-//   registrarUsuario: jest.fn(),
-//   inicioGoogle: jest.fn(),
-//   salirSesion: jest.fn(),
-//   actualizaPerfil: jest.fn(),
-// }));
-
-// describe('Funciones de Firebase', () => {
-//   afterEach(() => {
-//     jest.clearAllMocks();
-//   });
-
-//   describe('iniciaSesionUsuario', () => {
-//     it('debe llamar  correctos', () => {
-//       const email = 'prueba@gmail.com';
-//       const password = '123456';
-
-//       iniciaSesionUsuario(email, password);
-
-//       expect(iniciaSesionUsuario).toHaveBeenCalledWith(email, password);
-//     });
-//   });
-
-//   describe('registrarUsuario', () => {
-//     it('debe llamar a la tos', () => {
-//       // Configuración del mock o spy
-//       // eslint-disable-next-line no-unused-vars
-//       const createUserWithEmailAndPassword = jest.fn();
-//       const email = 'test@example.com';
-//       const password = 'password123';
-
-//       // Llamar a la función que se está probando
-//       registrarUsuario(email, password);
-
-//       // eslint-disable-next-line max-len
-//       // Verificar os parámetros correctos
-//       expect(registrarUsuario).toHaveBeenCalledWith(email, password);
-//     });
-//   });
-// });
-
 /* eslint-disable no-shadow */
-/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-import { getMaxListeners } from 'process';
 import { register } from '../src/components/register.js';
 import { registrarUsuario } from '../src/lib/firebase';
 import { onNavigate } from '../src/main';
 
-jest.mock('../src/lib/firebase');
-// jest.mock('../src/main');
+jest.mock('../src/lib/firebase', () => ({
+  registrarUsuario: jest.fn(),
+}));
+jest.mock('../src/main', () => ({
+  onNavigate: jest.fn(),
+}));
 
 function tick() {
   return new Promise((resolve) => {
@@ -85,7 +30,7 @@ describe('first Test for Register', () => {
   let createUser;
 
   beforeEach(() => {
-    document.body.appendChild(register());
+    document.body.replaceChildren(register(onNavigate));
     logMainSection = document.getElementById('idRegister');
     logInCont = document.querySelector('.registerContainer');
     containerInput = document.getElementById('regContainer');
@@ -109,18 +54,19 @@ describe('first Test for Register', () => {
     );
   });
 
-  it('Debería redireccionar a inicio', async () => {
-    registrarUsuario.mockImplementationOnce((email, pass) => {
+  // eslint-disable-next-line jest/no-focused-tests
+  it.only('Debería redireccionar a inicio', async () => {
+    registrarUsuario.mockImplementationOnce((email, pass) => { 
       return Promise.resolve({
         user: { email: 'hola@hola.com', password: '8520963' },
-      });
     });
+  });
 
-    // inputForEmail.value = 'email@verify.com';
-    // inputForPassword.value = '123456';
+    email.value = 'email@verify.com';
+    pass.value = '123456';
     createUser.click();
     await tick();
-    expect(onNavigate()).toHaveBeenCalledWith('/inicio');
+    expect(onNavigate).toHaveBeenCalledWith('/inicio');
 
     // expect(global.window.location.href).toContain('/inicio');
   });
